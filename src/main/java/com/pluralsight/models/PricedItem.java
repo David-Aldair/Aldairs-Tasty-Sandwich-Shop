097b0bc46e2648ba85a3d95f02a0da274e33c30d
David-Aldair <d.aldair.quinonez@gmail.com>
2025-11-11 19:39:25 -0500

    Implemented a pricedItem interface with a getPrice method.

--- src/main/java/com/pluralsight/models/PricedItem.java
+++ src/main/java/com/pluralsight/models/PricedItem.java
@@ -1,4 +1,11 @@
 package com.pluralsight.models;
 
+/*this class represents anything in the system hat has a price
+this allows us to put all the items in one pricedItem arraylist instead
+of putting them in 3 different lists(sandwiches, drinks, chips)*/
 public interface PricedItem {
+
+    /*gets the price of the item and also
+    returns it as a double*/
+    double getPrice();
 }
