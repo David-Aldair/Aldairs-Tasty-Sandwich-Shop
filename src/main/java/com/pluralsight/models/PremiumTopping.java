e85d29cfa4e65453d1e70abf49e485e33a8bb621
David-Aldair <d.aldair.quinonez@gmail.com>
2025-11-13 22:10:05 -0500

    Updated PremiumTopping with pricing and toString().

--- src/main/java/com/pluralsight/models/PremiumTopping.java
+++ src/main/java/com/pluralsight/models/PremiumTopping.java
@@ -14,4 +14,22 @@ public class PremiumTopping extends Topping{
         // super is calling the parent constructor to set name and extra
         super(name, extra);
     }
+
+    //if extra is true, the topping cost more, and then it returns the final price as a double
+    @Override
+    public double getPrice() {
+        if (isExtra()) {
+            return NORMAL_PRICE + EXTRA_PRICE;
+        } else {
+            return NORMAL_PRICE;
+        }
+    }
+
+    //this returns a readable string version of the topping with its price
+    @Override
+    public String toString() {
+
+        // super.toString gives you the name and extra part
+        return super.toString() + " - $" + String.format("%.2f", getPrice());
+    }
 }
