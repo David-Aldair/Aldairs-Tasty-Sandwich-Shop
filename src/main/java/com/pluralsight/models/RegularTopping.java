c266a6aa466c3958710f436e615e373340dd1ccd
David-Aldair <d.aldair.quinonez@gmail.com>
2025-11-13 21:59:52 -0500

    RegularTopping extends the Topping parent class. Includes normal and extra price constants. Implements constructor to initialize name and extra fields.

--- src/main/java/com/pluralsight/models/RegularTopping.java
+++ src/main/java/com/pluralsight/models/RegularTopping.java
@@ -1,4 +1,17 @@
 package com.pluralsight.models;
 
-public class RegularTopping {
+public class RegularTopping extends Topping {
+
+    //constants for pricing regular toppings
+    private static final double NORMAL_PRICE = 0.50;//normal price for a regular topping
+
+    //extra topping charge if needed
+    private static final double EXTRA_PRICE = 1.00;
+
+    //calling the superclass constructor to initialize the name and extra fields
+    public RegularTopping(String name, boolean extra) {
+
+        //calling the topping's constructor
+        super(name, extra);
+    }
 }
