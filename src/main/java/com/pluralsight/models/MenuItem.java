4bd2d3ae343b5a9cfe41085f2a8efeae11e88261
David-Aldair <d.aldair.quinonez@gmail.com>
2025-11-13 21:29:13 -0500

    Created a PremiumTopping and RegularTopping class. Also added MenuItem interface so all order items share a common getPrice() structure.

--- src/main/java/com/pluralsight/models/MenuItem.java
+++ src/main/java/com/pluralsight/models/MenuItem.java
@@ -3,7 +3,7 @@ package com.pluralsight.models;
 /*this class represents anything in the system hat has a price
 this allows us to put all the items in one pricedItem arraylist instead
 of putting them in 3 different lists(sandwiches, drinks, chips)*/
-public interface PricedItem {
+public interface MenuItem {
 
     /*gets the price of the item and also
     returns it as a double*/
