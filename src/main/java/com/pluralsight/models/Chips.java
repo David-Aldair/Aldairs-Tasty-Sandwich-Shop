5eb4ce44a0ee0db97e2c4f06b6186504cb6faa24
David-Aldair <d.aldair.quinonez@gmail.com>
2025-11-13 23:23:59 -0500

    Chips class implementing MenuItem with properties, constructor, getPrice(), and toString().

--- src/main/java/com/pluralsight/models/Chips.java
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -1,4 +1,31 @@
 package com.pluralsight.models;
 
-public class Chips {
+public class Chips implements MenuItem {
+
+    //properties for chips
+    private String name;
+    private double price;
+
+    //constructor
+    public Chips(String name){
+        this.name = name;
+        this.price = 1.50;
+    }
+
+    //getter for chip name
+    public String getName() {
+        return name;
+    }
+
+    //returning the price of the chips
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    //toString method returns a readable summary of the chips
+    @Override
+    public String toString() {
+        return name + " Chips - $" + String.format("%.2f", price);
+    }
 }
